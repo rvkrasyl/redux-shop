87a1ea7654e6b684212c9d8145f3ab41ca2cb075
rvkrasyl <iamdniwe56@gmail.com>
2022-12-22 00:41:38 +0200

    update array items in Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,36 @@ const initialState = {
     error: null,
     cartItems: [],
     orderTotal: 0
-}
+};
+
+const updateCartItem = (book, item = {}) => {
+    const { 
+        id = book.id, 
+        count = 0, 
+        title = book.title, 
+        total = 0 } = item;
+    
+    return {
+        id,
+        title,
+        count: count + 1,
+        total: Number(total) + Number(book.price)
+    }
+};
+
+const updateCartItems = (cartItems, newItem, idx) =>  {
+    if (idx === -1) {
+        return [
+            ...cartItems,
+            newItem
+        ]
+    }
+    return [
+        ...cartItems.slice(0, idx),
+        newItem,
+        ...cartItems.slice(idx + 1)
+    ]
+};
 
 const reducer = (state = initialState, action) => {   
     switch (action.type) {
@@ -32,21 +61,19 @@ const reducer = (state = initialState, action) => {
             };
         case "BOOK_ADDED_TO_CART" :
             const bookId = action.payload;
-            const book = state.books.find((book) => book.id === bookId);
-            const newItem = {
-                id: bookId,
-                name: book.title,
-                count: 1,
-                total: book.price
-            }
+            const book = state.books.find(({ id }) => id === bookId);
+            
+            const cartItemFromState = state.cartItems.find(({ id }) => id === bookId);
+            const cartItemFromStateIndex = state.cartItems.findIndex(({ id }) => id === bookId);
+
+            const newItem = updateCartItem(book, cartItemFromState);
+            
             return {
                 ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    newItem
-                ],
-                orderTotal: state.orderTotal + newItem.count * newItem.total
+                cartItems: updateCartItems(state.cartItems, newItem,cartItemFromStateIndex),
+                orderTotal: Number(state.orderTotal) + Number(book.price)
             };
+            
         default: 
             return state;
     }
