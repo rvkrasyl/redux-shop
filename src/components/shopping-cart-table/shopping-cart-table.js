01446def9853067cefe5fc03fdf4ca3b4b27f408
rvkrasyl <rvkrasyl@gmail.com>
2022-12-21 18:56:21 +0200

    add new component to Redux

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./shopping-cart-table.css";
 
-const ShoppingCartTable = () => {
+const ShoppingCartTable = ({ items, total,
+        onIncrease, onDecrease, onDelete }) => {
+    
+    const renderRow = (item, idx) => {
+        const { id, name, count, total } = item;
+        return (
+            <tr key={id}>
+                <td>{idx + 1}</td>
+                <td>{name}</td>
+                <td>{count}</td>
+                <td>${total}</td>
+                <td>
+                    <button 
+                    onClick={() => onIncrease(id)}
+                    className="btn btn-outline-success">
+                        <i className="fa-solid fa-circle-plus float-right" />
+                    </button>
+                    <button 
+                    onClick={() => onDecrease(id)}
+                    className="btn btn-outline-warning">
+                        <i className="fa-solid fa-circle-minus float-right" />
+                    </button>
+                    <button 
+                    onClick={() => onDelete(id)}
+                    className="btn btn-outline-danger">
+                        <i className="fa-solid fa-trash float-right" />
+                    </button>
+                </td>
+            </tr>
+        )
+    };
+
     return (
         <div className="shopping-cart-table">
             <h2>Your order</h2>
@@ -11,37 +43,36 @@ const ShoppingCartTable = () => {
                         <th>#</th>
                         <th>Item</th>
                         <th>Count</th>
-                        <th>Price</th>
+                        <th>Total</th>
                         <th>Action</th>
                     </tr>
                 </thead>
 
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>TheBook</td>
-                        <td>3</td>
-                        <td>$41</td>
-                        <td>
-                            <button className="btn btn-outline-danger">
-                                <i className="fa-solid fa-trash" />
-                            </button>
-                            <button className="btn btn-outline-success">
-                                <i className="fa-solid fa-circle-plus" />
-                            </button>
-                            <button className="btn btn-outline-warning">
-                                <i className="fa-solid fa-circle-minus" />
-                            </button>
-                        </td>
-                    </tr>
+                <tbody> 
+                    { items.map(renderRow) } 
                 </tbody>
             </table>
 
             <div className="total">
-                Total: $123
+                Total: ${total}
             </div>
         </div>
     );
-}
+};
 
-export default ShoppingCartTable;
+const mapStateToProps = ({ cartItems, orderTotal }) => {
+    return {
+        items: cartItems,
+        total: orderTotal
+    };
+};
+
+const mapDispatchToProps = () => {
+    return {
+        onIncrease: (id) => console.log(`Increase ${id}`),
+        onDecrease: (id) => console.log(`Decrease ${id}`),
+        onDelete: (id) => console.log(`Delete ${id}`)
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,25 +2,49 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: "Book1",
+            count: 3,
+            total: 150
+        },
+        {
+            id: 2,
+            name: "Book 2",
+            count: 4,
+            total: 120
+        },
+        {
+            id: 3,
+            name: "Book 3",
+            count: 1,
+            total: 70
+        },
+    ],
+    orderTotal: 340
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "FETCH_BOOKS_REQUEST" :
             return {
+                ...state,
                 books: [],
                 loading: true,
                 error: null
             };
         case "FETCH_BOOKS_SUCCESS" : 
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
             };
         case "FETCH_BOOKS_FAILURE" : {
             return {
+                ...state,
                 books: [],
                 loading: false,
                 error: action.payload
