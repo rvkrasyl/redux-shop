192e968470b64a9a2b83b3e38cb1212a219aa97c
rvkrasyl <rvkrasyl@gmail.com>
2022-12-21 13:21:52 +0200

    shopping-cart-table modified

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -7,29 +7,33 @@ const ShoppingCartTable = () => {
             <h2>Your order</h2>
             <table className="table">
                 <thead>
-                    <th>#</th>
-                    <th>Item</th>
-                    <th>Count</th>
-                    <th>Price</th>
-                    <th>Action</th>
+                    <tr>
+                        <th>#</th>
+                        <th>Item</th>
+                        <th>Count</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
                 </thead>
 
                 <tbody>
-                    <td>1</td>
-                    <td>TheBook</td>
-                    <td>3</td>
-                    <td>$41</td>
-                    <td>
-                        <button className="btn btn-outline-danger">
-                            <i class="fa-solid fa-trash" />
-                        </button>
-                        <button className="btn btn-outline-success">
-                            <i class="fa-solid fa-circle-plus" />
-                        </button>
-                        <button className="btn btn-outline-warning">
-                            <i class="fa-solid fa-circle-minus" />
-                        </button>
-                    </td>
+                    <tr>
+                        <td>1</td>
+                        <td>TheBook</td>
+                        <td>3</td>
+                        <td>$41</td>
+                        <td>
+                            <button className="btn btn-outline-danger">
+                                <i className="fa-solid fa-trash" />
+                            </button>
+                            <button className="btn btn-outline-success">
+                                <i className="fa-solid fa-circle-plus" />
+                            </button>
+                            <button className="btn btn-outline-warning">
+                                <i className="fa-solid fa-circle-minus" />
+                            </button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
 
