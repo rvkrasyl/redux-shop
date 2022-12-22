a168c54160c5fb481b1ca5784ce31591e32a0b5a
rvkrasyl <iamdniwe56@gmail.com>
2022-12-22 23:54:05 +0200

    redux middleware added

--- src/store.js
+++ src/store.js
@@ -1,7 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringMiddleware = () => (nextDispatch) => (action) => {
+    if (typeof action === "string") {
+        return nextDispatch({
+            type: action
+        });
+    }
+    return nextDispatch(action);
+};
+const logMiddleware = () => (nextDispatch) => (action) => {
+    console.log(action.type);
+    return nextDispatch(action);
+};
+
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
+store.dispatch("STRING_ACTION_HERE");
 
 export default store;
